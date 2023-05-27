57d1162a625e56d18df169da40f7ecdd4f4ac89d
Iago Franco <pattasiago@gmail.com>
2023-05-27 19:44:18 -0300

    add freight rule to FreightCalculator entity

--- test/FreightCalculator.test.ts
+++ test/FreightCalculator.test.ts
@@ -1,8 +1,20 @@
 import FreightCalculator from '../src/domain/entity/FreightCalculator';
 import Product from '../src/domain/entity/Product';
 
-test('Deve calcular o frete do produto', function () {
+test('Deve calcular o frete do produto com quantidade 1', function () {
   const product = new Product(6, 'A', 1000, 100, 30, 10, 3, 'USD');
   const freight = FreightCalculator.calculate(product);
   expect(freight).toBe(30);
+});
+
+test('Deve calcular o frete do produto com quantidade 3', function () {
+  const product = new Product(6, 'A', 1000, 100, 30, 10, 3, 'USD');
+  const freight = FreightCalculator.calculate(product, 3);
+  expect(freight).toBe(90);
+});
+
+test('Deve calcular o frete do produto com preco minimo', function () {
+  const product = new Product(6, 'A', 1000, 100, 30, 10, 0.9, 'USD');
+  const freight = FreightCalculator.calculate(product);
+  expect(freight).toBe(10);
 });
